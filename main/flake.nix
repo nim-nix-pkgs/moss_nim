32db3ea8bb86c4dca47a27bff6e9b16da4c0d82c
hugosenari <hugosenari@gmail.com>
2022-03-12 03:35:08 -0300

    chore: re index moss_nim

---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Moss (Measure of Software Similarity) implementation in Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-moss_nim-main.flake = false;
+  inputs.src-moss_nim-main.owner = "D4D3VD4V3";
+  inputs.src-moss_nim-main.ref   = "refs/heads/main";
+  inputs.src-moss_nim-main.repo  = "moss_nim";
+  inputs.src-moss_nim-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-moss_nim-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-moss_nim-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
